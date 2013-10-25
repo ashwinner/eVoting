7827b5ba9753110a85418863204b7fcf8c7a9ff9
Ashwin Lakshmanan <ashwinner92@gmail.com>
2013-10-25 12:32:03 +0530

    adding method createCandidateList

--- src/utils/FileOperations.java
+++ src/utils/FileOperations.java
@@ -1,26 +1,54 @@
 package utils;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FileOperations {
 
 	
 	public static Map<String, String> createEmailToPasskeyMap(String filename) throws IOException {
+		
 		Map<String, String> emailToPasskeyMap = new HashMap<String, String>();
-		File inputFile = new File(filename);
-		BufferedReader in = new BufferedReader(new FileReader(inputFile));
-		String line;
-		while((line=in.readLine())!=null) {
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		String inputLine;
+		
+		while(true) {
+			
+			inputLine = reader.readLine();
+			
+			if(inputLine==null || inputLine.equals(""))
+				break;
+			
 			//Assuming the file contains data of the format "<emailID> <passkey>"
-			String[] values = line.split(" ");
+			String[] values = inputLine.split(" ");
 			emailToPasskeyMap.put(values[0].trim(), values[1].trim());
 		}
-		in.close();
+		
+		reader.close();
 		return emailToPasskeyMap;
+	}
+	
+	public static List<String> createCandidateList(String filename) throws IOException {
+	
+		List<String> candidateList = new ArrayList<String>();
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+	
+		String inputLine;
+		
+		while(true) {
+			inputLine = reader.readLine();
+			
+			if(inputLine==null || inputLine.equals(""))
+				break;
+		
+			//Assuming the file contains one candidate per line
+			candidateList.add(inputLine.trim());
+		}
+		
+		reader.close();
+		return candidateList;
 	}
 }
