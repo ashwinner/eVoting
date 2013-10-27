500ff7bdc187bc27f590b6bd551ca6e268750201
reshma <itzreshma573@gmail.com>
2013-10-27 16:17:01 +0530

    deleted main

--- src/ballotgenerator/BallotGenerator.java
+++ src/ballotgenerator/BallotGenerator.java
@@ -1,11 +1,16 @@
 package ballotgenerator;
 
+import java.math.BigInteger;
+import java.security.interfaces.RSAPublicKey;
 import java.util.List;
+
+import authorizer.Authorizer;
 
 public class BallotGenerator {
 	
 	private static BallotGenerator instance=null;
 	private List<String> candidateList;
+	private Authorizer authorizer;
 	
 	public static BallotGenerator getInstance(List<String> candidateList) {
 		
@@ -19,21 +24,31 @@ public class BallotGenerator {
 	private BallotGenerator(List<String> candidateList) {
 		
 		this.candidateList=candidateList;
+		this.authorizer=Authorizer.getInstance();
 	}
 	
 	public List<String> requestBallot(String pvid) {
 		
 		if(!isValidPVID(pvid))
-			System.out.println("Invalid PVID");
-
-		return candidateList;
+			throw InvalidPVIDEception;
+		else
+			return candidateList;
+		
 	}
 
 	
-
-	private boolean isValidPVID(String pvid) {
+	
+	private boolean isValidPVID(String PVID) {
 		// TODO Auto-generated method stub
-		return true;
+		BigInteger pvid= new BigInteger(PVID);
+		RSAPublicKey authorizerPublicKey=authorizer.getPublicKey();
+		BigInteger e= authorizerPublicKey.getPublicExponent();
+		BigInteger n= authorizerPublicKey.getModulus();
+		BigInteger signVerifier= pvid.modPow(e, n);
+		if(signVerifier.toString().startsWith("1000"))
+			return true;
+		else
+			return false;
 	}
 
 }
